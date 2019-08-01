823e57639874f2195a6121f08646a4587b9f15b5
QuinB6248 <4_sAfJO25N@outlook.com>
2019-08-01 10:43:25 +0200

    First Commit

--- js/script.js
+++ js/script.js
@@ -1,73 +1,70 @@
 
-    const image = document.getElementById("image-sprite")
-    let changePos;
+const image = document.getElementById("image-sprite")
+let changePos;
 
-    function stopAnimate() {
-      clearInterval(changePos);
+function stopAnimate() {
+  clearInterval(changePos);
+}
+
+function animateScriptWalking () {
+  stopAnimate()
+  image.style.backgroundImage = 'url(./img/calm_sprite.png)';
+  let position = 0; //start position image sprite 
+  const positionOffset = 750; //position offset
+  changePos = setInterval (() => { 
+    image.style.backgroundPosition = `-${position}px 0px`; 
+    if (position < 29250) {
+      position += positionOffset //increment position 
+    } else {
+      stopAnimate()      
     }
+  }, 43)
+}
 
-    function animateScriptWalking () {
-      stopAnimate()
-      image.style.backgroundImage = 'url(./img/calm_sprite.png)';
-      let position = 0; //start position image sprite 
-      const positionOffset = 750; //position offset
-      changePos = setInterval (() => { 
-        image.style.backgroundPosition = `-${position}px 0px`; 
-        if (position < 29250) {
-          position += positionOffset //increment position 
-        } else {
-          stopAnimate()      
-        }
-      }, 43)
+function animateScriptCalm () {
+  stopAnimate()
+  image.style.backgroundImage = 'url(./img/calmer_sprite.png)';
+  let position = 0; //start position image sprite 
+  const positionOffset =750; //position offset
+  changePos = setInterval (() => { 
+    image.style.backgroundPosition = `-${position}px 0px`; 
+    if (position < 29250) {
+      position += positionOffset //increment position 
+    } else {
+      position = 0;        
     }
+  }, 43)
+}
 
-
-
-    function animateScriptCalm () {
-      stopAnimate()
-      image.style.backgroundImage = 'url(./img/calmer_sprite.png)';
-      let position = 0; //start position image sprite 
-      const positionOffset =750; //position offset
-      changePos = setInterval (() => { 
-        image.style.backgroundPosition = `-${position}px 0px`; 
-        if (position < 29250) {
-          position += positionOffset //increment position 
-        } else {
-          position = 0;        
-        }
-      }, 43)
+function animateScriptWorry () {
+  stopAnimate()
+  image.style.backgroundImage = 'url(./img/worry_sprite.png)';
+  let position = 0; //start position image sprite 
+  const positionOffset = 750; //position offset
+  changePos = setInterval (() => { 
+    image.style.backgroundPosition = `-${position}px 0px`; 
+    if (position < 22500) {
+      position += positionOffset //increment position 
+    } else {
+      position = 0;        
     }
+  }, 43)
+}
 
-
-    function animateScriptWorry () {
-      stopAnimate()
-      image.style.backgroundImage = 'url(./img/worry_sprite.png)';
-      let position = 0; //start position image sprite 
-      const positionOffset = 750; //position offset
-      changePos = setInterval (() => { 
-        image.style.backgroundPosition = `-${position}px 0px`; 
-        if (position < 22500) {
-          position += positionOffset //increment position 
-        } else {
-          position = 0;        
-        }
-      }, 43)
+function animateScriptStress () {
+  stopAnimate()
+  image.style.backgroundImage = 'url(./img/stress_sprite.png)';
+  let position = 0; //start position image sprite 
+  const positionOffset = 750; //position offset
+  changePos = setInterval (() => { 
+    image.style.backgroundPosition = `-${position}px 0px`; 
+    if (position < 29250) {
+      position += positionOffset //increment position 
+    } else {
+      position = 0;        
     }
-
-    function animateScriptStress () {
-      stopAnimate()
-      image.style.backgroundImage = 'url(./img/stress_sprite.png)';
-      let position = 0; //start position image sprite 
-      const positionOffset = 750; //position offset
-      changePos = setInterval (() => { 
-        image.style.backgroundPosition = `-${position}px 0px`; 
-        if (position < 29250) {
-          position += positionOffset //increment position 
-        } else {
-          position = 0;        
-        }
-      }, 43)
-    }
+  }, 43)
+}
 
 
 
